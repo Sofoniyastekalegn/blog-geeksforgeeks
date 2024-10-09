8f2fe9c8ae62b479c52b99869a3f6f95b6d50893
Sofoniyastekalegn <git@stackblitz.com>
2024-10-09 21:35:08 +0300

    Initial commit

---
+++ src/Banner.jsx
@@ -0,0 +1,9 @@
+// Banner.jsx
+
+export default function Banner() {
+  return (
+    <div className="h-32 w-full border-2 h-screen flex items-center justify-center bg-emerald-500 text-white">
+      <p className="text-2xl"> Welcome to geeksforgeeks Blog Page !!</p>
+      </div>
+  )
+}
